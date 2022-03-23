35aabc48ef1f274ce7947578025d0e7478d8d0b9
kakon4366 <kakonbarman4366@gmail.com>
2022-03-23 23:31:11 +0600

    Cart style added

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -5,6 +5,14 @@ const Cart = () => {
     return (
         <div className='cart'>
             <h2>Order Summary</h2>
+            <div className='cart-price'>
+                <p>Selected Items: </p>
+                <p>Total Price: </p>
+                <p>Total Shipping Charge: </p>
+                <p>Tax: </p>
+                <hr />
+                <h4>Grand Total: </h4>
+            </div>
         </div>
     );
 };
