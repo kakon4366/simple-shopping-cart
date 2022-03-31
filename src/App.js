0856b49ec1f2e616e87aabf1c5b8b3c151c84002
kakon4366 <kakonbarman4366@gmail.com>
2022-03-31 14:02:25 +0600

    React Router added and shop component add in Header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import './App.css';
-import Footer from './Components/Footer/Footer';
-import Header from './Components/Header/Header';
-import Shop from './Components/Shop/Shop';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+import Shop from "./Components/Shop/Shop";
 
 function App() {
-  return (
-    <div>
-     <Header></Header>
-     <Shop></Shop>
-     <Footer></Footer>
-    </div>
-  );
+	return (
+		<div>
+			<Header></Header>
+			<Routes>
+				<Route path="shop" element={<Shop></Shop>}></Route>
+			</Routes>
+			<Footer></Footer>
+		</div>
+	);
 }
 
 export default App;
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import logo from '../../logo.svg';
-import './Header.css';
+import React from "react";
+import logo from "../../logo.svg";
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <header className='header'>
-            <img src={logo} alt="logo" />
-            <nav className='nav-bar'>
-                <a href="/order">Order</a>
-                <a href="/order-review">Order Review</a>
-                <a href="manage-inventory">Manage Inventory</a>
-            </nav>
-        </header>
-    );
+	return (
+		<header className="header">
+			<img src={logo} alt="logo" />
+			<nav className="nav-bar">
+				<a href="/shop">Shop</a>
+				<a href="/order">Order</a>
+				<a href="/order-review">Order Review</a>
+				<a href="manage-inventory">Manage Inventory</a>
+			</nav>
+		</header>
+	);
 };
 
 export default Header;
