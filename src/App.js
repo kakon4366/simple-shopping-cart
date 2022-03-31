3406f3f78fcbf42e7a59d1cf04b891a20cddbe99
kakon4366 <kakonbarman4366@gmail.com>
2022-03-31 14:24:40 +0600

    Order component added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
 import Shop from "./Components/Shop/Shop";
 import NotFound404 from "./Components/NotFound404/NotFound404";
+import Order from "./Components/Order/Order";
 
 function App() {
 	return (
@@ -11,6 +12,7 @@ function App() {
 			<Header></Header>
 			<Routes>
 				<Route path="shop" element={<Shop></Shop>}></Route>
+				<Route path="order" element={<Order></Order>}></Route>
 				<Route path="*" element={<NotFound404></NotFound404>}></Route>
 			</Routes>
 			<Footer></Footer>
