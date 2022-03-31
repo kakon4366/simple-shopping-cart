d1259ab357daae6e4c05ad188843126941d2e081
kakon4366 <kakonbarman4366@gmail.com>
2022-03-31 14:48:50 +0600

    Order page update by cart and product custom hooks

--- src/Components/Order/Order.js
+++ src/Components/Order/Order.js
@@ -1,9 +1,23 @@
 import React from "react";
+import useCart from "../../Hooks/useCart";
+import useProduct from "../../Hooks/useProduct";
+import Cart from "../Cart/Cart";
+import ProductReview from "../ProductReview/ProductReview";
 
 const Order = () => {
+	const [products, setProducts] = useProduct([]);
+	const [cart, setCart] = useCart(products);
+	console.log(cart);
 	return (
-		<div>
-			<h2>This is order page</h2>
+		<div className="shop-container">
+			<div className="products-container">
+				{cart.map((product) => (
+					<ProductReview product={product}></ProductReview>
+				))}
+			</div>
+			<div className="cart-container">
+				<Cart cart={cart}></Cart>
+			</div>
 		</div>
 	);
 };
---
+++ src/Components/ProductReview/ProductReview.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductReview = () => {
+	return <div>This isi prlf</div>;
+};
+
+export default ProductReview;
