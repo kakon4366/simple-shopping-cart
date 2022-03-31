9aad4196caf11f565cdf291ad42f4dd910941494
kakon4366 <kakonbarman4366@gmail.com>
2022-03-31 15:20:10 +0600

    Product review component added

--- src/Components/ProductReview/ProductReview.js
+++ src/Components/ProductReview/ProductReview.js
@@ -1,7 +1,34 @@
 import React from "react";
+import "./ProductReview.css";
 
-const ProductReview = () => {
-	return <div>This isi prlf</div>;
+const ProductReview = ({ product }) => {
+	const { name, img, price, shipping, quantity } = product;
+	return (
+		<div className="review-product">
+			<div className="product-thumbnail">
+				<img src={img} alt="" />
+			</div>
+			<div className="product-detail-container">
+				<div className="product-details">
+					<h3>{name}</h3>
+					<p className="product-price">
+						Price: $<span>{price}</span>
+					</p>
+					<p>
+						<small>
+							Shipping: $<span>{shipping}</span>
+						</small>
+					</p>
+					<p>
+						<small>Quantity: {quantity}</small>
+					</p>
+				</div>
+				<div className="delete-button">
+					<button>Delete</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ProductReview;
